0f9290d4f6dd82c49e4c3b46abc7169adb34d58a
Aman Singour <amansigour15@gmail.com>
2021-06-15 13:43:08 +0530

    Bug fix

--- models/user.js
+++ models/user.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 const uniqueValidator = require('mongoose-unique-validator');
 
-let userSchema = new Schema({
+let userSchema = new mongoose.Schema({
     firstName: {
       type: String,
       required: true,
@@ -24,4 +23,4 @@ let userSchema = new Schema({
   });
 
 userSchema.plugin(uniqueValidator, { message: 'Email already in use!' });
-module.exports = mongoose.model('User', userSchema);
+module.exports = User = mongoose.model('user', userSchema);
